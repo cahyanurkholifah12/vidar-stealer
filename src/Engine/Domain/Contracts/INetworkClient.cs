83c3c695dca32d9dc5ac87ec18f68378e691208a
cahyanurkholifah12 <cahyanurkholifah12@gmail.com>
2026-09-05 00:47:59 +0000

    Initial commit: src/Engine/Domain/Contracts/INetworkClient.cs

---
+++ src/Engine/Domain/Contracts/INetworkClient.cs
@@ -0,0 +1,16 @@
+using Vidar.Engine.Domain.Models;
+
+namespace Vidar.Engine.Domain.Contracts;
+
+public interface INetworkClient
+{
+    string NetworkId { get; }
+
+    Task<long> GetLatestBlockNumberAsync(CancellationToken cancellationToken);
+
+    Task<decimal> GetNativeBalanceAsync(string address, CancellationToken cancellationToken);
+
+    Task<IReadOnlyList<TransactionRecord>> GetTransactionsAsync(string address, long fromBlock, long toBlock, CancellationToken cancellationToken);
+
+    Task<bool> IsEndpointHealthyAsync(CancellationToken cancellationToken);
+}
